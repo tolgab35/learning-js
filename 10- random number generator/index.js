79bdb40c2ec272199f405b71c276b6dd2eff7fa1
Tolga <tolgaboz350@gmail.com>
2023-12-14 23:52:43 +0300

    Add random number generator example

---
+++ 10- random number generator/index.js
@@ -0,0 +1,11 @@
+let x, y, z;
+
+document.getElementById("rollButton").onclick = function(){
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("xlabel").innerHTML = x;
+    document.getElementById("ylabel").innerHTML = y;
+    document.getElementById("zlabel").innerHTML = z;
+}
