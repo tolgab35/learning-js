9391dddd61734daeabc6b41091cc3242ce9f6a4f
Tolga <tolgaboz350@gmail.com>
2023-12-14 15:59:37 +0300

    Add const example

---
+++ 6- const/index.js
@@ -0,0 +1,12 @@
+// const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle: ");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is: ", circumference);
