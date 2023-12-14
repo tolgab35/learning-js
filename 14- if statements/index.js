10f116b72b105d6df93fa71ff3671ba498e56bcb
Tolga <tolgaboz350@gmail.com>
2023-12-15 00:54:04 +0300

    Add if statements example

---
+++ 14- if statements/index.js
@@ -0,0 +1,28 @@
+// if statement = a basic form of decision making
+//                if a condition is true, then do something
+//                if not, then don't do it!
+/*
+let age = window.prompt("How old are you?");
+
+if(age >= 65){
+    console.log("You are a senior citizen!");
+}
+else if(age >= 18){
+    console.log("You are an adult!");
+}
+else if(age < 0){
+    console.log("You haven't been born yet!");
+}
+else{
+    console.log("You are a child!");
+}
+*/
+
+let online = true;
+
+if(online){
+    console.log("You are online!");
+}
+else{
+    console.log("You are offline!");
+}
