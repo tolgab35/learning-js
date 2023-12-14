357642186e11ddb294256b7b780f777ec91cb803
Tolga <tolgaboz350@gmail.com>
2023-12-14 16:19:52 +0300

    Add math example

---
+++ 7- math/index.js
@@ -0,0 +1,17 @@
+let x = 3.14;
+
+// x = Math.round(x); // 3
+// x = Math.floor(x); // 3 (round down)
+// x = Math.ceil(x); // 4 (round up)
+// x = Math.pow(x, 2); // 9.8596
+x = Math.sqrt(x); // 1.772... (square root of x)
+// x = Math.abs(x, 2); // 3.14 absolute
+console.log(x);
+
+let a = Math.PI;
+let b = 5;
+let c = 9;
+let maximum = Math.max(a, b, c);
+let minimum = Math.min(a, b, c);
+
+console.log(maximum, minimum);
