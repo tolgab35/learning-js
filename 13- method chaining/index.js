d03b19cacf4ba623992322d9dd399acb81aa3ca1
Tolga <tolgaboz350@gmail.com>
2023-12-15 00:24:00 +0300

    Add method chaining example

---
+++ 13- method chaining/index.js
@@ -0,0 +1,8 @@
+// method chaining = calling one method after another
+//                   in one continious line of code
+
+let userName = " tolga";
+
+let letter = userName.slice(0, 3).toUpperCase().trim();
+
+console.log(letter);
