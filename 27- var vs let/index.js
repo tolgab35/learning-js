704c30ba36914d292d908fa741c62f106b72eea6
Tolga <tolgaboz350@gmail.com>
2023-12-15 03:34:52 +0300

    Add var and let differences

---
+++ 27- var vs let/index.js
@@ -0,0 +1,24 @@
+// variable scope = where a variable is accessible
+
+//  let = variables are limited to block scope {}
+//  var = variables are limited to a function(){}
+
+//global variable = is declared outside any function
+// (if global, var will change browser's window properties)
+
+/*
+  for (let i = 0; i <= 3; i++) {}
+  console.log(i); // 'i' is undefined here
+
+  for (var i = 0; i <= 3; i++) {}
+  console.log(i); // 'i' is defined here and 4
+*/
+
+doSomething();
+
+function doSomething() {
+  for (var i = 0; i <= 3; i++) {
+    //console.log(i);
+  }
+}
+console.log(i); // 'i' is undefined here (outside of function)
