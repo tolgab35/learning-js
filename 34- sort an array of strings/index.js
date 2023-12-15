46837eefca179f47293ec75ccd412f5a9d95c0c4
Tolga <tolgaboz350@gmail.com>
2023-12-15 10:48:47 +0300

    Add sort an array of strings example

---
+++ 34- sort an array of strings/index.js
@@ -0,0 +1,8 @@
+let fruits = ["banana", "apple", "orange", "mango"];
+
+fruits = fruits.sort(); // alphabetical order
+fruits = fruits.sort().reverse(); // reverse alphabetical order
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
