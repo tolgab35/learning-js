79aa1f7bac183fb4ac6b6be5c00efe4e91851301
Tolga <tolgaboz350@gmail.com>
2023-12-17 07:56:59 +0300

    Add getters and setters example

---
+++ 56- getters and setters/index.js
@@ -0,0 +1,30 @@
+// get = binds an object property to a function
+//       when that property is accessed
+
+class Car {
+  constructor(power) {
+    this._gas = 25;
+    this._power = power;
+  }
+  get power() {
+    return `${this._power}hp`;
+  }
+  get gas() {
+    return `${this._gas}L (${(this._gas / 50) * 100}%)`;
+  }
+  set gas(value) {
+    if (value > 50) {
+      value = 50;
+    } else if (value < 0) {
+      value = 0;
+    }
+    this._gas = value;
+  }
+}
+
+let car = new Car(400);
+
+car.gas = -100;
+
+console.log(car.power); // 400
+console.log(car.gas); // 0
