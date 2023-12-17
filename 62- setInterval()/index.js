f5b310be591076a8f3aa7642737a33d3036aab3c
Tolga <tolgaboz350@gmail.com>
2023-12-17 15:18:00 +0300

    Update setInterval() example

--- 62- setInterval()/index.js
+++ 62- setInterval()/index.js
@@ -2,14 +2,18 @@
 //                 asynchronous function (doesn't pause execution)
 
 let count = 0;
-let max = Number(window.prompt("Count up to what #?"));
+let max = window.prompt("Count up to what #?");
 
-const myTimer = setInterval(countUp, 1000);
+if (isNaN(max) || max === "") {
+  console.log("Invalid input. Please enter a valid number.");
+} else {
+  const myTimer = setInterval(countUp, 1000);
 
-function countUp() {
-  count += 1;
-  console.log(count);
-  if (count >= max) {
-    clearInterval(myTimer);
+  function countUp() {
+    count += 1;
+    console.log(count);
+    if (count >= max) {
+      clearInterval(myTimer);
+    }
   }
 }
