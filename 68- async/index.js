5117926282c17e6ee1957c16498da489c0bc48f1
Tolga <tolgaboz350@gmail.com>
2023-12-18 21:06:04 +0300

    Add async example

---
+++ 68- async/index.js
@@ -0,0 +1,15 @@
+// async = makes a function return a Promise
+
+async function loadFile() {
+  let fileLoaded = true;
+
+  if (fileLoaded) {
+    return "File loaded";
+  } else {
+    throw "File NOT loaded";
+  }
+}
+
+loadFile()
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error));
