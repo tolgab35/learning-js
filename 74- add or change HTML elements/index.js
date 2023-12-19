adefe29b3b3eed45ff6a3566a6c1a214b8cb9b9d
Tolga <tolgaboz350@gmail.com>
2023-12-19 14:23:14 +0300

    Add 'add or change HTML elements example'

---
+++ 74- add or change HTML elements/index.js
@@ -0,0 +1,15 @@
+// add/change HTML elements
+// .innerHTML (vulnerable to XSS attacks)
+// .textContent (more secure)
+/*
+const nameTag = document.createElement("h1");
+nameTag.innerHTML = window.prompt("Enter your name");
+document.body.append(nameTag);
+*/
+
+const myList = document.querySelector("#fruit");
+const listItem = document.createElement("li");
+listItem.textContent = "mango";
+// myList.append(listItem);
+// myList.prepend(listItem);
+myList.insertBefore(listItem, myList.getElementsByTagName("li")[0]);
