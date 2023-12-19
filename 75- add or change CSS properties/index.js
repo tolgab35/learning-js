c470644301b08ed3588186a3e86c29244f636f92
Tolga <tolgaboz350@gmail.com>
2023-12-19 14:30:20 +0300

    Add 'add or change CSS properties example'

---
+++ 75- add or change CSS properties/index.js
@@ -0,0 +1,7 @@
+const title = document.getElementById("myTitle");
+title.style.backgroundColor = "rgb(00,00,00)";
+title.style.color = "white";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid red";
+title.style.display = "block";
