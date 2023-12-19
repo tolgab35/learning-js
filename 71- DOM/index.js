c4c50761625b5e4b6c3762378a00e0c88b70779e
Tolga <tolgaboz350@gmail.com>
2023-12-19 11:15:25 +0300

    add DOM example

---
+++ 71- DOM/index.js
@@ -0,0 +1,12 @@
+// DOM = Documenet Object Model (API)
+//       An interface for changing the content of a page
+
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.URL);
+
+// document.title = "Title goes here!";
+// document.location = "https://www.google.com";
+// document.body.style.backgroundColor = "skyblue";
+
+document.getElementById("myDiv").innerHTML = "Hello";
