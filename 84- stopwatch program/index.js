c00b9455e06649fa5cafcd2f1792e25093565d3b
Tolga <tolgaboz350@gmail.com>
2023-12-20 19:00:30 +0300

    Add stopwatch program example

---
+++ 84- stopwatch program/index.js
@@ -0,0 +1,59 @@
+// selecting elements from the HTML
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let startTime = 0; // holds the timestamp when the timer starts
+let elapsedTime = 0; // tracks the time passed since starting
+let paused = true; // a flag to check if the timer is currently paused
+let intervalId; // store the ID returned by 'setInterval'
+
+// store the hours, minutes, and seconds of the timer.
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+startBtn.addEventListener("click", () => {
+  if (paused) {
+    paused = false;
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime, 100);
+  }
+});
+pauseBtn.addEventListener("click", () => {
+  if (!paused) {
+    paused = true;
+    elapsedTime = Date.now() - startTime;
+    clearInterval(intervalId);
+  }
+});
+resetBtn.addEventListener("click", () => {
+  paused = true;
+  clearInterval(intervalId);
+  startTime = 0;
+  elapsedTime = 0;
+  hrs = 0;
+  mins = 0;
+  secs = 0;
+  timeDisplay.textContent = "00:00:00";
+});
+
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
+  secs = Math.floor((elapsedTime / 1000) % 60);
+  mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+  secs = pad(secs);
+  mins = pad(mins);
+  hrs = pad(hrs);
+
+  timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+  function pad(unit) {
+    return ("0" + unit).length > 2 ? unit : "0" + unit;
+  }
+  console.log(startTime);
+  console.log(elapsedTime);
+}
